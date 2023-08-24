8bb4dffe8543c2e480b08237350acf5eec9aa5dd
Rishabh sharma <92917973+Rishabh32700@users.noreply.github.com>
2023-08-24 20:30:40 +0530

    emoji and leader board project added

---
+++ leaderBoard/leaderBoard.js
@@ -0,0 +1,58 @@
+const scoreMainContainer = document.getElementById("scoreMainContainer");
+const AddDetailsButton = document.getElementById("AddDetailsButton");
+console.log(AddDetailsButton);
+AddDetailsButton.addEventListener("click", () => {
+  const fName = document.getElementById("fName");
+  const lName = document.getElementById("lName");
+  const country = document.getElementById("country");
+  const score = document.getElementById("score");
+
+  const scoreBoard = document.createElement("div");
+  scoreBoard.classList.add("scoreBoard");
+
+  scoreBoard.innerHTML = `
+    <p class="playerName">${fName.value} ${lName.value}</p>
+    <p class="country">${country.value}</p>
+    <p class="score">${score.value}</p>
+    <p class="deleteIcon">&#x1f5d1;</p>
+  `;
+
+  scoreMainContainer.appendChild(scoreBoard);
+  fName.value = "";
+  lName.value = "";
+  country.value = "";
+  score.value = "";
+  sortBoard();
+});
+
+function sortBoard() {
+  const ScoreBoard = document.querySelectorAll(".scoreBoard");
+  const arr = [];
+  ScoreBoard.forEach((ele) => arr.push(ele));
+  const sortedArray = arr
+    .map((ele) => {
+      return ele;
+    })
+    .sort((a, b) => {
+      let runOfManOne = parseInt(a.children[2].textContent);
+      let runOfManTwo = parseInt(b.children[2].textContent);
+      // B = CURRENT
+      // A = PREV
+      // MAN 1 = PREV
+      // MAN 2 = CURRENT
+
+      // PREV IS COMMING UPAR
+      // PREV > CURRENT
+      if (runOfManOne > runOfManTwo) {
+        return -1;
+      }
+      // PREV < CURRENT
+      if (runOfManOne < runOfManTwo) {
+        return 1;
+      }
+    });
+
+  sortedArray.forEach((ele) => {
+    scoreMainContainer.append(ele);
+  });
+}
